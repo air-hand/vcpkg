7da5b86cd002e4df09bc3f870292c4ba993f97d0
air-hand <45233435+air-hand@users.noreply.github.com>
2026-05-11 19:22:24 +0900

    fix: add patch vs2026

--- ports/opencv4/portfile.cmake
+++ ports/opencv4/portfile.cmake
@@ -30,7 +30,7 @@ vcpkg_from_github(
       0021-fix-qt-gen-def.patch
       0022-android-use-vcpkg-cpu-features.patch
       0023-ffmpeg8-support.patch
-      1000-cuda-standard.patch
+      1000-cuda-std-and-vs2026.patch
       "${PATCH1_FILE}"
 )
 # Disallow accidental build of vendored copies
